a41f107f3f587b62290dde9c7f18eb818e8764e3
mattjohnson32 <m36johnson@student.bridgew.edu>
2019-10-21 23:35:48 -0400

    finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class Main {//
     public static Random ran;
     public static void main(String[] args) throws IOException {
         ran = new Random();
